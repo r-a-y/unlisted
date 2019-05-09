1bd9665f47d21a0aa223bda4dbc089f84367dbe0
r-a-y <r-a-y@gmx.com>
2019-05-09 14:15:34 -0700

    Gutenberg: If post visibility changes to 'Public' or 'Password Protected', remove our unlisted marker.
    Requires using wp.data.subscribe to listen to post visibility changes
    and to our unlisted postmeta marker.
    
    See #6.

--- assets/gutenberg.js
+++ assets/gutenberg.js
@@ -1,4 +1,4 @@
-( function( wp, $ ) {
+( function( wp ) {
 	var registerPlugin = wp.plugins.registerPlugin;
 	var PluginPostStatusInfo = wp.editPost.PluginPostStatusInfo;
 	var el = wp.element.createElement;
@@ -7,7 +7,36 @@
 	var checkBox = wp.components.CheckboxControl;
 	var withSelect = wp.data.withSelect;
 	var withDispatch = wp.data.withDispatch;
+	var subscribe = wp.data.subscribe;
 	var compose = wp.compose.compose;
+
+	// If visibility is not 'private', remove our unlisted marker.
+	// TODO: subscribe runs far too often, this is a Gutenberg thing though...
+	subscribe( function() {
+		var newVisibility = wp.data.select( 'core/editor' ).getEditedPostVisibility();
+		var unlisted = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'meta' );
+
+		// data.subscribe runs too early for meta to be set early, so set defaults.
+		if ( typeof unlisted === 'undefined' ) {
+			unlisted = false;
+		}
+		if ( ! unlisted.hasOwnProperty( 'ray_unlisted' ) ) {
+			unlisted = false;
+		} else {
+			unlisted = unlisted.ray_unlisted;
+		}
+
+		/*
+		 * Only remove our unlisted postmeta if visibility is not private and our
+		 * unlisted postmeta was previously true.
+		 */
+		if ( typeof newVisibility !== 'undefined' && 'private' !== newVisibility && true === unlisted ) {
+			wp.data.dispatch( 'core/editor' ).editPost( {
+				meta: { ['ray_unlisted']: false }
+			} );
+		}
+
+	} );
 
 	var metaCheckbox = compose(
 		withDispatch( function( dispatch, props ) {
@@ -57,4 +86,4 @@
 			);
 		},
 	} );
-} )( window.wp, jQuery );
+} )( window.wp );
